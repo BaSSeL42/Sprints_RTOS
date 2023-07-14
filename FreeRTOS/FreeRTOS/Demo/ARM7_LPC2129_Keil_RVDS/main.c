1b51773fe628015f160db6114a750480e2fff03b
Mahmoud Matarawy <30804086+MahmoudMatarawy@users.noreply.github.com>
2023-07-15 02:32:55 +0300

    update

--- FreeRTOS/FreeRTOS/Demo/ARM7_LPC2129_Keil_RVDS/main.c
+++ FreeRTOS/FreeRTOS/Demo/ARM7_LPC2129_Keil_RVDS/main.c
@@ -60,10 +60,51 @@
 #include "FreeRTOS.h"
 #include "task.h"
 #include "lpc21xx.h"
-
+#include "semphr.h"
 /* Peripheral includes. */
 #include "serial.h"
 #include "GPIO.h"
+
+#include "Botton.h"
+
+#define BOTTON_1									1
+#define BOTTON_2									2
+#define BOTTON_1_PERIOD									50
+#define BOTTON_2_PERIOD									50
+#define Periodic_Transmitter_PERIOD						100
+#define Uart_Receiver_PERIOD						20
+#define Load_1_Simulation_PERIOD						10
+#define Load_2_Simulation_PERIOD						100
+
+
+#define FAILING_EDGE			2
+#define RISING_EDGE				1
+
+#define PRESSED						1
+#define NOT_PRESSED				0
+#define SEND_STRING				1
+
+
+unsigned char		botton_1_state_for_consumer_task		= NOT_PRESSED;
+unsigned char		botton_2_state_for_consumer_task		= NOT_PRESSED;
+
+
+static signed char botton_1_rising_string[] = "Botton 1 RISING EDGE\n";
+static signed char botton_1_failing_string[] = "Botton 1 FAILING EDGE\n";
+static unsigned char botton_1_rising_string_size = 9 + 13;
+static unsigned char botton_1_failing_string_size = 9 + 14;
+static signed char botton_2_rising_string[] = "Botton 2 RISING EDGE\n";
+static signed char botton_2_failing_string[] = "Botton 2 FAILING EDGE\n";
+static unsigned char botton_2_rising_string_size = 9 + 13;
+static unsigned char botton_2_failing_string_size = 9 + 14;
+
+static signed char string_1[]="Thank\n";
+static unsigned char string_1_size = 7;
+unsigned char		botton_1_state		= PRESSED;
+unsigned char		botton_1_prv_state		= PRESSED;
+unsigned char		botton_2_state		= PRESSED;
+unsigned char		botton_2_prv_state		= PRESSED;
+static unsigned char   consumer_task_data [4];
 
 
 /*-----------------------------------------------------------*/
@@ -75,12 +116,6 @@
 #define mainCOM_TEST_BAUD_RATE	( ( unsigned long ) 115200 )
 
 
-TaskHandle_t Button1TaskHandler 							= NULL;
-TaskHandle_t Button2TaskHandler 							= NULL;
-TaskHandle_t Periodic_Transmitter_TaskHandler = NULL;
-TaskHandle_t Uart_Receiver_TaskHandler			  = NULL;
-TaskHandle_t Load_1_Simulation_TaskHandler 		= NULL;
-TaskHandle_t Load_2_Simulation_TaskHandler 		= NULL;
 
 
 /*******************************************************************************************************/
@@ -112,7 +147,13 @@ void vApplicationIdleHook( void )
 	
 }
 */
-
+TaskHandle_t Button_1_Monitor_Handler = NULL;
+TaskHandle_t Button_2_Monitor_Handler = NULL;
+TaskHandle_t Periodic_Transmitter_Handler = NULL;
+TaskHandle_t Uart_Receiver_Handler = NULL;
+TaskHandle_t Load_1_Simulation_Handler = NULL;
+TaskHandle_t Load_2_Simulation_Handler = NULL;
+SemaphoreHandle_t Semaphore_Handler;
 
 
 void Button_1_Monitor( void * pvParameters )
@@ -120,8 +161,26 @@ void Button_1_Monitor( void * pvParameters )
 
     for( ;; )
     {
-			
-			
+			botton_1_state = botton_read(BOTTON_1);					// read botton status
+			if(botton_1_state != botton_1_prv_state)
+				{
+					//botton_1_state_for_consumer_task = CHANGE_EDGE;
+					if(botton_1_state == PRESSED)
+					{
+						consumer_task_data[0] = RISING_EDGE;
+					}
+					else{
+						consumer_task_data[0] = FAILING_EDGE;
+					}
+						
+					//consumer_task_data
+				}
+				else
+				{
+					// do nothing
+				}
+			botton_1_prv_state = botton_1_state;
+			vTaskDelay(BOTTON_1_PERIOD);									// block the task
     }
 }
 
@@ -131,7 +190,23 @@ void Button_2_Monitor( void * pvParameters )
 
     for( ;; )
     {
-			
+			botton_2_state = botton_read(BOTTON_2);					// read botton status
+			if(botton_2_state != botton_2_prv_state)
+				{
+					if(botton_2_state == PRESSED)
+					{
+						consumer_task_data[1] = RISING_EDGE;
+					}
+					else{
+						consumer_task_data[1] = FAILING_EDGE;
+					}
+				}
+				else
+				{
+					// do nothing
+				}
+			botton_2_prv_state = botton_2_state;
+			vTaskDelay(BOTTON_2_PERIOD);									// block the task
     }
 }
 
@@ -141,8 +216,17 @@ void Button_2_Monitor( void * pvParameters )
 void Periodic_Transmitter( void * pvParameters )
 {
 
-    for( ;; )
-    {
+    //static unsigned char semaphore_state = pdFALSE ;
+		for(;;)
+		{
+			consumer_task_data[2] = SEND_STRING;
+			/*semaphore_state = xSemaphoreTake( Semaphore_Handler, ( TickType_t ) 0 );
+			if(semaphore_state == pdTRUE)
+			{
+				vSerialPutString(string_1, string_1_size);
+				xSemaphoreGive( Semaphore_Handler);
+			}*/
+			vTaskDelay(Periodic_Transmitter_PERIOD);									// block the task
     }
 }
 
@@ -150,26 +234,91 @@ void Periodic_Transmitter( void * pvParameters )
 
 void Uart_Receiver( void * pvParameters )
 {
+	static unsigned char semaphore_state = pdFALSE ;
     for( ;; )
     {
+			if(consumer_task_data[0] == RISING_EDGE)
+			{
+				semaphore_state = xSemaphoreTake( Semaphore_Handler, ( TickType_t ) 0 );
+				if(semaphore_state == pdTRUE)
+				{
+					vSerialPutString(botton_1_rising_string, botton_1_rising_string_size);
+					xSemaphoreGive( Semaphore_Handler);
+				}
+				consumer_task_data[0] = 0;
+			}
+			else if(consumer_task_data[0] == FAILING_EDGE)
+			{
+				semaphore_state = xSemaphoreTake( Semaphore_Handler, ( TickType_t ) 0 );
+				if(semaphore_state == pdTRUE)
+				{
+					vSerialPutString(botton_1_failing_string, botton_1_failing_string_size);
+					xSemaphoreGive( Semaphore_Handler);
+				}
+				consumer_task_data[0] = 0;
+			}
 			
+			if(consumer_task_data[1] == RISING_EDGE)
+			{
+				semaphore_state = xSemaphoreTake( Semaphore_Handler, ( TickType_t ) 0 );
+				if(semaphore_state == pdTRUE)
+				{
+					vSerialPutString(botton_2_rising_string, botton_2_rising_string_size);
+					xSemaphoreGive( Semaphore_Handler);
+				}
+				consumer_task_data[1] = 0;
+			}
+			else if(consumer_task_data[1] == FAILING_EDGE)
+			{
+				semaphore_state = xSemaphoreTake( Semaphore_Handler, ( TickType_t ) 0 );
+				if(semaphore_state == pdTRUE)
+				{
+					vSerialPutString(botton_2_failing_string, botton_2_failing_string_size);
+					xSemaphoreGive( Semaphore_Handler);
+				}
+				consumer_task_data[1] = 0;
+			}
+			
+			if(consumer_task_data[2] == SEND_STRING)
+			{
+				semaphore_state = xSemaphoreTake( Semaphore_Handler, ( TickType_t ) 0 );
+				if(semaphore_state == pdTRUE)
+				{
+					vSerialPutString(string_1, string_1_size);
+					xSemaphoreGive( Semaphore_Handler);
+				}
+				consumer_task_data[2] = 0;
+			}
+			vTaskDelay(Uart_Receiver_PERIOD);									// block the task
     }
 }
 
-
+// 9990
 void Load_1_Simulation( void * pvParameters )
 {
     for( ;; )
     {
+			unsigned int i = 0;
+			for(i = 0 ; i < 9990 ; i++)
+			{
+				// heavy load for 5 ms
+			}
+			vTaskDelay(Load_1_Simulation_PERIOD);									// block the task
     }
 }
 
 
-
+// 23000
 void Load_2_Simulation( void * pvParameters )
 {
     for( ;; )
     {
+			unsigned int i = 0;
+			for(i = 0 ; i < 23000 ; i++)
+			{
+				// heavy load for 12 ms
+			}
+			vTaskDelay(Load_2_Simulation_PERIOD);									// block the task
     }
 }
 
@@ -183,19 +332,65 @@ int main( void )
 	/* Setup the hardware for use with the Keil demo board. */
 	prvSetupHardware();
 
-
+	Semaphore_Handler = xSemaphoreCreateMutex();
+	xSemaphoreGive( Semaphore_Handler );
     /* Create Tasks here */
 	/* todo: Create the task, storing the handle. */
 		  xTaskPeriodicCreate(
-                    TASK_REFERENCE,       			/* Function that implements the task. */
+                    Button_1_Monitor,       			/* Function that implements the task. */
                     "Task_Name",       		/* Text name for the task. */
                     configMINIMAL_STACK_SIZE,      				  		/* Stack size in words, not bytes. */
                     ( void * ) 0,       		/* Parameter passed into the task. */
                     1,                  		/* Priority at which the task is created. */
-                    &TASK_HANDLER, 					/* Used to pass out the created task's handle. */
+                    &Button_1_Monitor_Handler, 					/* Used to pass out the created task's handle. */
 										50 );   								// Setting Deadline
 			
-			
+			xTaskPeriodicCreate(
+                    Button_2_Monitor,       			/* Function that implements the task. */
+                    "Task_Name",       		/* Text name for the task. */
+                    configMINIMAL_STACK_SIZE,      				  		/* Stack size in words, not bytes. */
+                    ( void * ) 0,       		/* Parameter passed into the task. */
+                    2,                  		/* Priority at which the task is created. */
+                    &Button_2_Monitor_Handler, 					/* Used to pass out the created task's handle. */
+										50 );   								// Setting Deadline
+
+			xTaskPeriodicCreate(
+                    Periodic_Transmitter,       			/* Function that implements the task. */
+                    "Task_Name",       		/* Text name for the task. */
+                    configMINIMAL_STACK_SIZE,      				  		/* Stack size in words, not bytes. */
+                    ( void * ) 0,       		/* Parameter passed into the task. */
+                    2,                  		/* Priority at which the task is created. */
+                    &Periodic_Transmitter_Handler, 					/* Used to pass out the created task's handle. */
+										100 );   								// Setting Deadline		
+
+			xTaskPeriodicCreate(
+                    Uart_Receiver,       			/* Function that implements the task. */
+                    "Task_Name",       		/* Text name for the task. */
+                    configMINIMAL_STACK_SIZE,      				  		/* Stack size in words, not bytes. */
+                    ( void * ) 0,       		/* Parameter passed into the task. */
+                    2,                  		/* Priority at which the task is created. */
+                    &Uart_Receiver_Handler, 					/* Used to pass out the created task's handle. */
+										20 );   								// Setting Deadline		
+
+
+			xTaskPeriodicCreate(
+                    Load_1_Simulation,       			/* Function that implements the task. */
+                    "Task_Name",       		/* Text name for the task. */
+                    configMINIMAL_STACK_SIZE,      				  		/* Stack size in words, not bytes. */
+                    ( void * ) 0,       		/* Parameter passed into the task. */
+                    2,                  		/* Priority at which the task is created. */
+                    &Load_1_Simulation_Handler, 					/* Used to pass out the created task's handle. */
+										10 );   								// Setting Deadline
+
+			xTaskPeriodicCreate(
+                    Load_2_Simulation,       			/* Function that implements the task. */
+                    "Task_Name",       		/* Text name for the task. */
+                    configMINIMAL_STACK_SIZE,      				  		/* Stack size in words, not bytes. */
+                    ( void * ) 0,       		/* Parameter passed into the task. */
+                    2,                  		/* Priority at which the task is created. */
+                    &Load_2_Simulation_Handler, 					/* Used to pass out the created task's handle. */
+										100 );   								// Setting Deadline
+
 
 										
 									
